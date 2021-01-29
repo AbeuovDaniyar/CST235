891252cc1ccb42e71465e29466853a4a1e8f9b61
AbeuovDaniyar <70968370+AbeuovDaniyar@users.noreply.github.com>
2021-01-28 19:54:22 -0700

    Add files via upload
    assignment 2a

---
+++ assignment2a/src/assignment/TestServlet.java
@@ -0,0 +1,62 @@
+package assignment;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");
+		if(firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty()) {
+			request.getRequestDispatcher("TestError.jsp").forward(request, response);
+		}else {
+		
+		request.setAttribute("firstname", firstName);
+		request.setAttribute("lastname", lastName);
+		request.getRequestDispatcher("TestRespnose.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+}
